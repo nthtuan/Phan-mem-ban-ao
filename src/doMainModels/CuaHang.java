febe028df2a41276dd5de1ca77966ae96c6a1ddd
ngocthanh <dangngocthanh720@gmail.com>
2022-11-12 15:05:01 +0700

    Tạo class CuaHang tương tác với bảng Cửa hàng trong database

---
+++ src/doMainModels/CuaHang.java
@@ -0,0 +1,73 @@
+package doMainModels;
+
+import java.util.UUID;
+
+public class CuaHang {
+    private UUID id;
+    private String ma;
+    private String ten;
+    private String diaChi;
+    private String thanhPho;
+    private int trangThai;
+
+    public CuaHang() {
+    }
+
+    
+    public CuaHang(UUID id, String ma, String ten, String diaChi, String thanhPho, int trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.diaChi = diaChi;
+        this.thanhPho = thanhPho;
+        this.trangThai = trangThai;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getThanhPho() {
+        return thanhPho;
+    }
+
+    public void setThanhPho(String thanhPho) {
+        this.thanhPho = thanhPho;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    } 
+}
