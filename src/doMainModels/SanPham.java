5c433939cddd472ca27beffe6b43ea9f81e82b6d
nthtuan <nthtuan03092003@gmail.com>
2022-11-12 17:12:44 +0700

    tao Sanpham de tuong tac voi db

---
+++ src/doMainModels/SanPham.java
@@ -0,0 +1,55 @@
+
+package doMainModels;
+
+import java.util.UUID;
+
+
+public class SanPham {
+    private UUID id;
+    private String ma;
+    private String ten;
+    private int trangThai;
+
+    public SanPham() {
+    }
+
+    public SanPham(UUID id, String ma, String ten, int trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.trangThai = trangThai;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+}
