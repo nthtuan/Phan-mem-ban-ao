101e3ab5945d005db7520e35078a299a995011f7
ngocthanh <dangngocthanh720@gmail.com>
2022-11-12 15:06:03 +0700

    Tạo class KhachHang tương tác với bảng khách hàng trong database

---
+++ src/doMainModels/KhachHang.java
@@ -0,0 +1,92 @@
+package doMainModels;
+
+import java.util.UUID;
+
+public class KhachHang {
+    private UUID id;
+    private String ma;
+    private String hoTen;
+    private String gioiTinh;
+    private String soDienThoai;
+    private String diaChi;
+    private String email;
+    private int trangThai;
+
+    public KhachHang() {
+    }
+
+    public KhachHang(UUID id, String ma, String hoTen, String gioiTinh, String soDienThoai, String diaChi, String email, int trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.hoTen = hoTen;
+        this.gioiTinh = gioiTinh;
+        this.soDienThoai = soDienThoai;
+        this.diaChi = diaChi;
+        this.email = email;
+        this.trangThai = trangThai;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+}
